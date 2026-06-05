0aaaa0af49bbc4bbfdea2c2a17c1fcf35053319f
Kayla Garnett <kgarnettk01@gmail.com>
2026-06-05 13:29:54 +1200

    Ensuring the graphs on the home page and view data page are cohesive using CSS file

--- MyVitals/src/main/java/com/group1/myvitals/controller/HomePageController.java
+++ MyVitals/src/main/java/com/group1/myvitals/controller/HomePageController.java
@@ -4,7 +4,7 @@ import com.group1.myvitals.view.SceneManager;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
 public class HomePageController {
     @FXML private Label HomePageTodaysDate;
     @FXML private Label HomePageNumberOfDataPointsRecorded;
-    @FXML private StackedBarChart<String, Number> HomePageDataPointsInLastMonthGraph;
+    @FXML private LineChart<String, Number> HomePageDataPointsInLastMonthGraph;
     @FXML private Button HomePageRecordDataButton;
     @FXML private Button HomePageViewDataButton;
     @FXML private Button HomePageCreateReportButton;
@@ -23,15 +23,17 @@ public class HomePageController {
     @FXML private Button HomePageLogoutButton;
 
     private LocalDate today;
-    private DateTimeFormatter dateFormatter;
+    private DateTimeFormatter mainDateFormatter;
+    private DateTimeFormatter graphDateFormatter;
 
     @FXML
     public void initialize() {
         // Display todays date
         today = LocalDate.now();
-        dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String formattedDate = today.format(dateFormatter);
-        HomePageTodaysDate.setText(formattedDate);
+        mainDateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String mainFormattedDate = today.format(mainDateFormatter);
+        HomePageTodaysDate.setText(mainFormattedDate);
+        graphDateFormatter = DateTimeFormatter.ofPattern("dd/MM");
 
         // Display number of data points collected today
         updateNumberOfDataPointsRecorded();
@@ -78,7 +80,7 @@ public class HomePageController {
         // Clear current data
         HomePageDataPointsInLastMonthGraph.getData().clear();
 
-        // Create 3 categories of data points 
+        // Create 4 categories of data points 
         XYChart.Series<String, Number> heartRate = new XYChart.Series<>();
         heartRate.setName("Heart Rate");
 
@@ -88,24 +90,29 @@ public class HomePageController {
         XYChart.Series<String, Number> bloodPressure = new XYChart.Series<>();
         bloodPressure.setName("Blood Pressure");
 
+        XYChart.Series<String, Number> respRate = new XYChart.Series<>();
+        respRate.setName("Respiratory Rate");
+
         // Add data for each day to the different data series
         for (int i = 29; i >= 0; i--) {
-            LocalDate targetDate = today.minusDays(i);
-            String xValueDate = targetDate.format(dateFormatter);
+            String date = today.minusDays(i).format(graphDateFormatter);
             
-            int heartRateCount = 1; // Placeholder TO DO!!  replace with call to database?
+            int heartRateCount = 3; // Placeholder TO DO!!  replace with call to database?
             int sleepCount = 1; // Placeholder TO DO!!  replace with call to database?
-            int bpCount = 1; // Placeholder TO DO!!  replace with call to database?
+            int bpCount = 2; // Placeholder TO DO!!  replace with call to database?
+            int respRateCount = 4; // Placeholder TO DO!!  replace with call to database?
 
             // Populate each series with the number of data points for each day
-            heartRate.getData().add(new XYChart.Data<>(xValueDate, heartRateCount));
-            sleepDuration.getData().add(new XYChart.Data<>(xValueDate, sleepCount));
-            bloodPressure.getData().add(new XYChart.Data<>(xValueDate, bpCount));
+            heartRate.getData().add(new XYChart.Data<>(date, heartRateCount));
+            sleepDuration.getData().add(new XYChart.Data<>(date, sleepCount));
+            bloodPressure.getData().add(new XYChart.Data<>(date, bpCount));
+            respRate.getData().add(new XYChart.Data<>(date, respRateCount));
         }
 
         // Add all data to graph
         HomePageDataPointsInLastMonthGraph.getData().add(heartRate);
         HomePageDataPointsInLastMonthGraph.getData().add(sleepDuration);
         HomePageDataPointsInLastMonthGraph.getData().add(bloodPressure);
+        HomePageDataPointsInLastMonthGraph.getData().add(respRate);
     }
 }
--- MyVitals/src/main/java/com/group1/myvitals/controller/ViewDataPageController.java
+++ MyVitals/src/main/java/com/group1/myvitals/controller/ViewDataPageController.java
@@ -11,9 +11,9 @@ import java.time.format.DateTimeFormatter;
 
 public class ViewDataPageController {
     @FXML private StackedBarChart<String, Number> ViewDataBloodPressureGraph;
-    @FXML private LineChart ViewDataHeartRateGraph;
-    @FXML private LineChart ViewDataSleepDurationGraph;
-    @FXML private LineChart ViewDataSleepDurationGraph2;
+    @FXML private LineChart<String, Number> ViewDataHeartRateGraph;
+    @FXML private LineChart<String, Number> ViewDataSleepDurationGraph;
+    @FXML private LineChart<String, Number> ViewDataRespRateGraph;
 
     private LocalDate today;
     private DateTimeFormatter dateFormatter;
@@ -21,7 +21,11 @@ public class ViewDataPageController {
     @FXML
     public void initialize() {
         today = LocalDate.now();
-        dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        dateFormatter = DateTimeFormatter.ofPattern("dd/MM");
+        updateViewDataBloodPressureGraph();
+        updateViewDataHeartRateGraph();
+        updateViewDataSleepDurationGraph();
+        updateViewDataRespRateGraph();
     }
 
     public void updateViewDataBloodPressureGraph(){
@@ -36,20 +40,80 @@ public class ViewDataPageController {
         systolic.setName("Systolic");
 
         // Add data for each day to the different data series
-        for (int i = 29; i >= 0; i--) {
-            LocalDate targetDate = today.minusDays(i);
-            String xValueDate = targetDate.format(dateFormatter);
+        for (int i = 13; i >= 0; i--) {
+            String date = today.minusDays(i).format(dateFormatter);
             
             int diastolicValue = 1; // Placeholder TO DO!!  replace with call to database?
-            int systolicValue = 1; // Placeholder TO DO!!  replace with systolic number minus diastolic value to get the difference
+            int rawSystolicValue = 2; // Placeholder TO DO!!  replace with call to database?
+            int systolicValue = rawSystolicValue - diastolicValue; 
 
-            // Populate each series with the number of data points for each day
-            diastolic.getData().add(new XYChart.Data<>(xValueDate, diastolicValue));
-            systolic.getData().add(new XYChart.Data<>(xValueDate, systolicValue));
+            // Populate the different series with data for that day
+            diastolic.getData().add(new XYChart.Data<>(date, diastolicValue));
+            systolic.getData().add(new XYChart.Data<>(date, systolicValue));
         }
         // Add all data to graph
         ViewDataBloodPressureGraph.getData().add(diastolic);
         ViewDataBloodPressureGraph.getData().add(systolic);
+    }
+
+    public void updateViewDataHeartRateGraph(){
+        // Clear current data
+        ViewDataHeartRateGraph.getData().clear();
+
+        // Create a category for data 
+        XYChart.Series<String, Number> heartRate = new XYChart.Series<>();
+
+        // Add data for each day to the series
+        for (int i = 13; i >= 0; i--) {
+            String date = today.minusDays(i).format(dateFormatter);
+
+            int heartRateValue = 60; // Placeholder TO DO!!  replace with call to database
+
+            // Populate series with data for that day
+            heartRate.getData().add(new XYChart.Data<>(date, heartRateValue));
+        }
+        // Add data to graph
+        ViewDataHeartRateGraph.getData().add(heartRate);
+    }
+
+    public void updateViewDataSleepDurationGraph(){
+        // Clear current data
+        ViewDataSleepDurationGraph.getData().clear();
+
+        // Create a category for data 
+        XYChart.Series<String, Number> sleepDuration = new XYChart.Series<>();
+
+        // Add data for each day to the series
+        for (int i = 13; i >= 0; i--) {
+            String date = today.minusDays(i).format(dateFormatter);
+
+            double sleepDurationValue = 8.5; // Placeholder TO DO!!  replace with call to database
+
+            // Populate series with data for that day
+            sleepDuration.getData().add(new XYChart.Data<>(date, sleepDurationValue));
+        }
+        // Add data to graph
+        ViewDataSleepDurationGraph.getData().add(sleepDuration);
+    }
+
+    public void updateViewDataRespRateGraph(){
+        // Clear current data
+        ViewDataRespRateGraph.getData().clear();
+
+        // Create a category for data 
+        XYChart.Series<String, Number> respRate = new XYChart.Series<>();
+
+        // Add data for each day to the series
+        for (int i = 13; i >= 0; i--) {
+            String date = today.minusDays(i).format(dateFormatter);
+
+            int respRateValue = 15; // Placeholder TO DO!!  replace with call to database
+
+            // Populate series with data for that day
+            respRate.getData().add(new XYChart.Data<>(date, respRateValue));
+        }
+        // Add data to graph
+        ViewDataRespRateGraph.getData().add(respRate);
     }
 }
 
